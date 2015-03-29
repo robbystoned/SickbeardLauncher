5da68b2cfc13948c50078fb3009e45018110584a
robbystoned <robbystoned@gmail.com>
2015-03-29 20:07:22 +0200

    basics now done
    the basic program now works, the icon, stopping and starting sickbeard

--- SickbeardLauncher/AppDelegate.swift
+++ SickbeardLauncher/AppDelegate.swift
@@ -2,9 +2,7 @@
 //  AppDelegate.swift
 //  SwiftStatusBarApplication
 //
-//  Created by Tommy Leung on 6/7/14.
-//  Copyright (c) 2014 Tommy Leung. All rights reserved.
-//
+
 
 import Cocoa
 
@@ -13,9 +11,9 @@ class AppDelegate: NSObject, NSApplicationDelegate
 {
     @IBOutlet var window: NSWindow?
     @IBOutlet var popover : NSPopover?
-    
     @IBOutlet weak var menu: NSMenu!
-   
+    @IBOutlet weak var StatusItem: NSMenuItem!
+    let task = NSTask()
     let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
     
     func applicationDidFinishLaunching(aNotification: NSNotification?)
@@ -24,8 +22,29 @@ class AppDelegate: NSObject, NSApplicationDelegate
         icon?.setTemplate(true)
         statusItem.image = icon
         statusItem.menu = menu
+        startSickbeard()
+    }
+    
+    func startSickbeard(){
+
+        task.launchPath = "/usr/bin/python"
+        task.arguments = ["/Applications/sickbeard/sickbeard.py"]
+        task.launch()
+        
+    }
+
+    @IBAction func quitApplication(sender: NSMenuItem) {
+        //stop the sickbeard process and quit this app
+        task.terminate();
+        //exit(0)
+    }
+    @IBAction func openSettingsWindow(sender: AnyObject) {
+        //open settings window
+    }
+    
+    @IBAction func restartSickbeard(sender: NSMenuItem) {
     }
 
     
-   }
+}
 
